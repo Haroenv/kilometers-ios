03679775a6d06fb2b2bfbe87e271ec36570399cf
Haroen Viaene <hello@haroen.me>
2016-05-04 21:06:02 +0200

    city

--- ios/FirstViewController.swift
+++ ios/FirstViewController.swift
@@ -16,22 +16,49 @@ class FirstViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var distance: UILabel!
     var locationManager: CLLocationManager!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Create a location manager object
+        self.locationManager = CLLocationManager()
         
-
+        // Set the delegate
+        self.locationManager.delegate = self
         
-        if (CLLocationManager.locationServicesEnabled()) {
-            locationManager = CLLocationManager()
-            locationManager.delegate = self;
-            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.startUpdatingLocation()
+        getQuickLocationUpdate()
+    }
+    
+    func currentCity(location: CLLocation) {
+        let geoCoder = CLGeocoder()
+        geoCoder.reverseGeocodeLocation(location) {
+            (placemarks, error) -> Void in
+            
+            let placeArray = placemarks as [CLPlacemark]!
+            
+            var placeMark: CLPlacemark!
+            placeMark = placeArray?[0]
+            if let current = placeMark.addressDictionary?["City"] as? NSString {
+                self.city.text = current as String
+                self.distance.text = "\(0) km" as String
+            }
         }
     }
     
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func getQuickLocationUpdate() {
+        // Request location authorization
+        self.locationManager.requestWhenInUseAuthorization()
         
+        // Request a location update
+        self.locationManager.requestLocation()
+        // Note: requestLocation may timeout and produce an error if authorization has not yet been granted by the user
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Process the received location update
         let location = locations.last! as CLLocation
         
         let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
@@ -39,7 +66,8 @@ class FirstViewController: UIViewController, CLLocationManagerDelegate {
         
         map.setRegion(region, animated: true)
         map.showsUserLocation = true
+        
+        currentCity(location);
     }
-    
 }
 
