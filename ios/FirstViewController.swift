93d088fdcd4912a7586e66e8b56f7463d3563ab6
Haroen Viaene <hello@haroen.me>
2016-05-04 18:51:06 +0200

    things that may or may not work

--- ios/FirstViewController.swift
+++ ios/FirstViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        
+        map.addAnnotation(annotation)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
